1f8b40eca8ff0b6a3ecafd5f6ca917b396239f37
BICH-CPU <117829928+BICH-CPU@users.noreply.github.com>
2025-02-23 01:02:42 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrationController;
+use App\Http\Controllers\ContactController;
+use App\Http\Controllers\CommentController;
+
 
 
 // routes/web.php
@@ -27,9 +30,9 @@ Route::get('/scholarship', function () {
     return view('user.scholarship.index'); // Đường dẫn đến view
 });
 
-Route::get('/register', function () {
-    return view('user.register.index'); // Đường dẫn đến view
-});
+// Route::get('/register', function () {
+//     return view('user.register.index'); // Đường dẫn đến view
+// });
 
 Route::get('/news', function () {
     return view('user.news.index'); // Đường dẫn đến view
@@ -39,9 +42,30 @@ Route::get('/intro', function () {
     return view('user.introduce.index'); // Đường dẫn đến view
 });
 
-Route::get('/contact', function () {
-    return view('user.contact.index'); // Đường dẫn đến view
+Route::get('/register', function () {
+    return view('user.registration.index'); // Đường dẫn đến view
 });
+
+Route::get('/admin', function () {
+    return view('admin.index'); // Đường dẫn đến view
+});
+
+Route::get('/ad-comment', function () {
+    return view('admin.comments.index', compact('comments')); // Truyền vào view
+});
+Route::post('/comments', [CommentController::class, 'store'])->name('comments.store');
+Route::get('/comments', [CommentController::class, 'index'])->name('comments.index');
+Route::post('/comments/{id}/edit', [CommentController::class, 'update'])->name('comments.update');
+Route::delete('/comments/{id}', [CommentController::class, 'destroy'])->name('comments.destroy');
+
+// Route dành cho user (khách có thể bình luận)
+Route::post('/comments/store', [CommentController::class, 'store'])->name('comments.store');
+
+// Route dành cho admin (quản lý bình luận)
+Route::delete('/admin/comments/delete/{id}', [CommentController::class, 'destroy']);
+Route::get('/ad-comment', [CommentController::class, 'adminIndex'])->name('admin.comments');
+
+Route::get('/home', [CommentController::class, 'index'])->name('home');
 
 
 
